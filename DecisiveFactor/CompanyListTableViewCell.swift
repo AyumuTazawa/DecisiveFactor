26e172133574362b40daf827af6572fec104215f
AyumuTazawa <tazawaayumu@gmail.com>
2020-09-06 13:30:13 +0900

    画像表示機能追加

--- DecisiveFactor/CompanyListTableViewCell.swift
+++ DecisiveFactor/CompanyListTableViewCell.swift
@@ -7,28 +7,38 @@
 //
 
 import UIKit
+import Firebase
+import Nuke
 
 class CompanyListTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var industry: UILabel!
     @IBOutlet weak var companyName: UILabel!
     @IBOutlet weak var ShadowView: UIView!
-
+    @IBOutlet weak var cellBackgroundView: UIView!
+    @IBOutlet weak var companyImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-       
+        
     }
+    
     
     func setCell(company: CompayListData) {
-           self.companyName.text = company.companyName as String
+        self.companyName.text = company.companyName as String
         self.industry.text = company.industry as String
+        if let url = URL(string: company.companyImageUrl ?? "" ) {
+            Nuke.loadImage(with: url, into: self.companyImage)
             
-       }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-  
+        }
+        
     }
     
-   
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    
+    
 }
