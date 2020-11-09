e171ba0fe3c8617b82e134da5fea296c49939842
AyumuTazawa <tazawaayumu@gmail.com>
2020-11-09 22:43:35 +0900

    引数名修正

--- DecisiveFactor/AddReviewsViewController.swift
+++ DecisiveFactor/AddReviewsViewController.swift
@@ -14,7 +14,7 @@ class AddReviewsViewController: UIViewController, UITextViewDelegate {
     
     var database: Firestore!
     var selectedCompanyData: CompayListData!
-    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var companyName: UILabel!
     @IBOutlet weak var addReviewsTextView: UITextView!
     
     override func viewDidLoad() {
@@ -22,7 +22,7 @@ class AddReviewsViewController: UIViewController, UITextViewDelegate {
         
         database = Firestore.firestore()
         addReviewsTextView.delegate = self
-        self.name.text = selectedCompanyData.companyName
+        self.companyName.text = selectedCompanyData.companyName
         
     }
     
@@ -34,8 +34,8 @@ class AddReviewsViewController: UIViewController, UITextViewDelegate {
     @IBAction func addReviews(_ sender: Any) {
         firstly {
             self.fechUserData()
-        }.done { Udata in
-            self.upReviewData(userData: Udata)
+        }.done { writeUserData in
+            self.upReviewData(userData: writeUserData)
         }.catch { err in
             print(err)
         }
@@ -49,8 +49,8 @@ class AddReviewsViewController: UIViewController, UITextViewDelegate {
                     print(err)
                 } else {
                     guard let data = documentSnapshot?.data() else { return }
-                    let Udata = UserData.init(udata: data)
-                    resolver.fulfill(Udata)
+                    let writeUserData = UserData.init(udata: data)
+                    resolver.fulfill(writeUserData)
                 }
             }
         }
@@ -62,8 +62,10 @@ class AddReviewsViewController: UIViewController, UITextViewDelegate {
             let writeUser = userData.name
             let writeUserId = userData.userId
             let companyId = self.selectedCompanyData.postId
-            let addreview = ["review": writeReview, "writeUser": writeUser,"writeUserId": writeUserId, "companyId": companyId]
-            Firestore.firestore().collection("Reviews").document().setData(addreview) { (err) in
+            let saveReview = Firestore.firestore().collection("reviews").document()
+            let reviewId = saveReview.documentID
+            let addreviewData = ["review": writeReview, "writeUser": writeUser,"writeUserId": writeUserId, "companyId": companyId, "reviewId": reviewId]
+            saveReview.setData(addreviewData) { (err) in
                 if let err = err {
                     print(err)
                 } else {
